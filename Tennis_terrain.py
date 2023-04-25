4d84672b41528a9185354b4538d49d971332d7ea
Paul BARBARIN <contact@paulbarbarin.com>
2023-04-25 12:02:53 +0200

    Working on main file

--- Tennis_terrain.py
+++ Tennis_terrain.py
@@ -1,26 +1,30 @@
 from matplotlib import pyplot as plt
 import numpy as np
 
-class robot:                               #class item
-    def __init__(self,position,direction=0,mv_speed=0,rot_speed=0):          #id and price are atributes of the item class
-        self.position=position
-        self.direction=direction
-        self.mv_speed=mv_speed
-        self.rot_speed=rot_speed
+
+class robot:
+    def __init__(self, position, direction, mv_speed, rot_speed):
+        self.position = position
+        self.direction = direction
+        self.mv_speed = mv_speed
+        self.rot_speed = rot_speed
 
     def __str__(self):
-        return "The robot is in "+str(self.position)+" in direction "+str(self.direction)+" at a mv speed "+str(self.mv_speed)+" and rot speed "+str(self.rot_speed)
+        return "The robot is in " + str(self.position) + " in direction " + str(
+            self.direction) + " at a mv speed " + str(self.mv_speed) + " and rot speed " + str(self.rot_speed)
 
 
-def read_world_file(N, filename):
+def init_world(n, filename):
+
     global robot
-    
-    list_balls=[]
-    
-    size=0
 
-    plt.xlim(0, N)
-    plt.ylim(0, N)
+    list_balls = []
+
+    size = 0
+
+    plt.xlim(0, n)
+    plt.ylim(0, n)
+
     with open(filename, 'r') as f:
 
         for line in f:
@@ -38,32 +42,39 @@ def read_world_file(N, filename):
             if name == 'R':
                 x, y = map(int, coords[1:-1].split(','))
                 plt.plot(x, y, marker="o", label='robot')
-                robot=robot((x,y),0,4)  ###############################################################
+                robot = robot((x, y), 0, 4, 0, 0)
 
             elif name.isdigit():
                 x, y = map(int, coords[1:-1].split(','))
-                plt.plot(x, y, marker="o", label='balls')
-                size+=1
-                list_balls.append((x,y))
+                plt.plot(x, y, marker="o", label='ball')
+                size += 1
+                list_balls.append((x, y))
 
-    # plt.show()
-    print(len(list_balls))
+    print("World generated : Number of balls to pick : " + str(len(list_balls)))
+
+    x_values = []
+    y_values = []
+
     for i in range(len(list_balls)):
-        for j in range(i+1, len(list_balls)):
-            x_values = [list_balls[i][0], list_balls[j][0]]
-            y_values = [list_balls[i][1], list_balls[j][1]]
-            plt.plot(x_values, y_values, 'b-')  # 'b-' means blue lines
-            value = abs(list_balls[i][0] - list_balls[j][0]) + abs(list_balls[i][1] - list_balls[j][1])
-            plt.text((list_balls[i][0] + list_balls[j][0])/2, (list_balls[i][1] + list_balls[j][1])/2, str(value))
 
-    G=np.zeros((size,size))
+        x_values.append(list_balls[i][0])
+        y_values.append(list_balls[i][1])
 
-    return G,robot,list_balls
+        plt.plot(x_values, y_values, 'b-')
+
+        for j in range(len(list_balls)):
+            if j != i:
+                value = abs(list_balls[i][0] - list_balls[j][0]) + abs(list_balls[i][1] - list_balls[j][1])
+                plt.text((list_balls[i][0] + list_balls[j][0]) / 2, (list_balls[i][1] + list_balls[j][1]) / 2, str(value))
+
+    G = np.zeros((size, size))
+
+    return G, robot, list_balls
 
 
 if __name__ == "__main__":
     N = 30
-    G = read_world_file(
+    G = init_world(
         N, '/home/geekboyboss/Tennis-ball-gathering-robot/terrain.csv')
 
     plt.legend()
---
+++ sujet.md
@@ -0,0 +1,4 @@
+# Robot ramasseur de balles de tennis
+
+## Partie 1
+Nous proposons de modéliser le monde avec un graphe complet 
