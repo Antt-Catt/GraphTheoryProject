69365c85e43b3683399816540dfc528b72e35358
Mohamed KHERRAZ <Mohamed.Kherraz@bordeaux-inp.fr>
2023-05-11 21:54:49 +0200

    adding commented graph for clarification

--- graph.py
+++ graph.py
@@ -1,36 +1,32 @@
 import numpy as np
 import itertools
 
-
-
 # Initializes the graph
 def init_graph(list_nodes, robot):
-
-    list_nodes = list_nodes+[robot.position]
+    list_nodes = list_nodes + [robot.position]
     size = len(list_nodes)
 
     # First fill with zeros
     graph = np.zeros((size, size, size))
 
-    # Foreach couple (i,j) where i different j initialize lenght to -1
+    # Foreach couple (i,j) where i different j initialize length to -1
     for layer in range(len(graph)):
         for i in range(len(graph[0])):
             for j in range(len(graph[0])):
                 if i != j:
                     graph[layer][i][j] = -1
 
-    # If from pos equals to end pos set lenght to 0
+    # If from pos equals to end pos set length to 0
     for layer in range(len(graph)):
         for i in range(len(graph[0])):
             for j in range(len(graph[0])):
                 if i == layer or j == layer:
                     if (i == (size - 1) or j == (size - 1)):
-                        # graph[layer][i][j] = weight((list_nodes[layer] - [0, 1]), i, j, list_nodes, robot)
                         graph[layer][i][j] = 0
                     else:
                         graph[layer][i][j] = 0
 
-    # filling the graph with angles
+    # Filling the graph with angles
     for layer in range(len(graph)):
         for i in range(len(graph[0])):
             for j in range(len(graph[0])):
@@ -44,7 +40,7 @@ def init_graph(list_nodes, robot):
 
 # Calculate the time it takes to go from current to future node coming from previous node.
 def weight(previous_node, current_node, future_node, list_nodes, robot):
-    # calculate angle between 3 points a,b,c
+    # Calculate angle between 3 points a, b, c
     if isinstance(previous_node, int):
         a = list_nodes[previous_node]
     else:
@@ -55,21 +51,18 @@ def weight(previous_node, current_node, future_node, list_nodes, robot):
     ba = b - a
     bc = c - b
 
-    # print(np.linalg.norm(ba), np.linalg.norm(bc))
-
     cosine_angle = np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
-    if (cosine_angle > 1):
+    if cosine_angle > 1:
         cosine_angle = 1
-    elif (cosine_angle < -1):
+    elif cosine_angle < -1:
         cosine_angle = -1
     angle = np.arccos(cosine_angle)
 
-    # distance between current and future point
+    # Distance between current and future point
     distance = np.linalg.norm(bc)
-    # print("angle =", angle, "; distance =", distance)
     return angle / robot.rot_speed + distance / robot.mv_speed
 
-
+# Finds the optimal path by going through the tree of paths
 def path_opt(graph, list_balls, robot):
     init_pos = robot.position
     perm = itertools.permutations(range(len(list_balls)))
@@ -108,64 +101,67 @@ def path_opt(graph, list_balls, robot):
         else:
             weight_min = weight
             path_min = path
-            
 
     print(weight_min)
     return path_min
 
 
+# Finds the shortest path going from a node to the closest next node
 def shortest_path(graph, list_balls, robot):
-    path_min = []
+    path_min = []  # Initialize the list to store the minimum path
     init_pos = robot.position
-    list_nodes = list_balls+[init_pos] + \
-        [np.array([init_pos[0], init_pos[1]-0.5])]
-    passed_balls = [np.array([init_pos[0], init_pos[1]-0.5])]
+    list_nodes = list_balls + [init_pos] + \
+        [np.array([init_pos[0], init_pos[1] - 0.5])]
+    passed_balls = [np.array([init_pos[0], init_pos[1] - 0.5])]
     already_chosen_balls = []
+    # Initialize variables to track previous, current, and next nodes
     (previous, current, next) = (-1, -1, -1)
 
-    sum=0
+    sum = 0  # Initialize the sum of weights
 
-    min = float('inf')
+    min = float('inf')  # Initialize the minimum weight as infinity
     for i in range(len(list_balls)):
-        # print(i,"is",weight(len(list_nodes)-1,len(list_nodes)-2,i,list_nodes,robot))
-        if (weight(len(list_nodes)-1, len(list_nodes)-2, i, list_nodes, robot) < min):
-            min = weight(len(list_nodes)-1, len(list_nodes) -
+        # Find the ball with the minimum weight from the initial position
+        if weight(len(list_nodes) - 1, len(list_nodes) - 2, i, list_nodes, robot) < min:
+            min = weight(len(list_nodes) - 1, len(list_nodes) -
                          2, i, list_nodes, robot)
-            (previous, current, next) = (len(list_nodes)-1, len(list_nodes)-2, i)
-            sum+=min
+            (previous, current, next) = (
+                len(list_nodes) - 1, len(list_nodes) - 2, i)
+            sum += min
 
     passed_balls.append(init_pos)
     already_chosen_balls.append(current)
     already_chosen_balls.append(next)
 
     goto_node = 0
-    while (len(already_chosen_balls)-1 < len(list_balls)):
-        #print("selecting in :",layer_selector)
+    while len(already_chosen_balls) - 1 < len(list_balls):
+        # Iterate until all balls are chosen
         layer = graph[current]
         weights = layer[next]
         min = float('inf')
-        for i in range(len(weights)-1):
+        for i in range(len(weights) - 1):
             if i not in already_chosen_balls:
+                # Find the ball with the minimum weight from the current ball
                 if weights[i] < min:
                     min = weights[i]
                     goto_node = i
-                    sum+=min
+                    sum += min
 
         (previous, current, next) = (current, next, goto_node)
         already_chosen_balls.append(next)
 
-
     already_chosen_balls.append(already_chosen_balls[0])
-    path_min=[list_nodes[i] for i in already_chosen_balls]
-    print(sum)
+    path_min = [list_nodes[i]
+                for i in already_chosen_balls]  # Construct the minimum path
+    print(sum)  # Print the total weight of the path
     return path_min
 
 
 if __name__ == "__main__":
     from world import *
 
-    robot,list_balls=init_world("terrain.csv")
-    graph = init_graph(list_balls,robot)
-    path=shortest_path(graph,list_balls,robot)
+    robot, list_balls = init_world("terrain.csv")
+    graph = init_graph(list_balls, robot)
+    path = shortest_path(graph, list_balls, robot)
     print(path)
     print("This file is not runable\n")
